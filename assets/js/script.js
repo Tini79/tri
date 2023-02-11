8ff19f9170bae71dd8443c0d43ff1f1a32b99792
putri <antariputri26@gmail.com>
2023-02-11 21:32:19 +0800

    added some css, js and fixed some html codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log('script')
+let sliderImg = document.querySelector(".slider-img")
+let images = ["scoupsmind.png", "jeonghanmind.png", "joshuamind.png",
+    "junmind.png", "hoshimind.png", "wonwoomind.png", "woozimind.png", "the8mind.png", "mingyumind.png", "dkmind.png", "seungkwanmind.png", "vernonmind.png", "dinomind.png"]
+let i = 0
+
+function prev() {
+    if (i <= 0) i = images.length;
+    i--;
+
+    return setImage();
+}
+
+function next() {
+    if(i >= images.length - 1) i = -1;
+    i++;
+
+    return setImage(); 
+}
+
+function setImage() {
+    return sliderImg.setAttribute("src", "../assets/images/" + images[i]);
+}
