a3fe762623669af0fc256e4709cb3f2ea9a23849
putri <antariputri26@gmail.com>
2023-03-05 01:31:55 +0800

    done :D

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,25 @@
-let sliderImg = document.querySelector(".slider-img")
-let images = ["scoupsmind.png", "jeonghanmind.png", "joshuamind.png",
-    "junmind.png", "hoshimind.png", "wonwoomind.png", "woozimind.png", "the8mind.png", "mingyumind.png", "dkmind.png", "seungkwanmind.png", "vernonmind.png", "dinomind.png"]
-let i = 0
+// let sliderImg = document.querySelector(".slider-img")
+// let images = ["scoupsmind.png", "jeonghanmind.png", "joshuamind.png",
+//     "junmind.png", "hoshimind.png", "wonwoomind.png", "woozimind.png", "the8mind.png", "mingyumind.png", "dkmind.png", "seungkwanmind.png", "vernonmind.png", "dinomind.png"]
+// let i = 0
 
-function prev() {
-    if (i <= 0) i = images.length;
-    i--;
+// function prev() {
+//     if (i <= 0) i = images.length;
+//     i--;
 
-    return setImage();
-}
+//     return setImage();
+// }
 
-function next() {
-    if(i >= images.length - 1) i = -1;
-    i++;
+// function next() {
+//     if(i >= images.length - 1) i = -1;
+//     i++;
 
-    return setImage(); 
-}
+//     return setImage(); 
+// }
 
-function setImage() {
-    return sliderImg.setAttribute("src", "../assets/images/" + images[i]);
-}
+// function setImage() {
+//     return sliderImg.setAttribute("src", "../assets/images/" + images[i]);
+// }
 
 window.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
